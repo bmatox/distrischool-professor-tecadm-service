8ad3ce3d7cb86bd902cf2bebc99a7bc1e3436de7
51644 <51644@tjce.jus.br>
2025-10-12 19:07:08 -0300

    feat: cria Professor entity e repository

---
+++ src/main/java/br/com/distrischool/professortecadm/model/Professor.java
@@ -0,0 +1,31 @@
+package br.com.distrischool.professortecadm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "professores")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true) 
+    private String email;
+
+    private String especialidade;
+
+    private LocalDate dataContratacao;
+
+}
---
+++ src/main/java/br/com/distrischool/professortecadm/repository/ProfessorRepository.java
@@ -0,0 +1,10 @@
+package br.com.distrischool.professortecadm.repository;
+
+import br.com.distrischool.professortecadm.model.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+}
