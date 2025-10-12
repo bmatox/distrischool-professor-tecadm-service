dfe1332938d0446a180cabf6bb9d3a4e0a3e907e
51644 <51644@tjce.jus.br>
2025-10-12 18:51:52 -0300

    git commit -m "feat: Initial commit do servico professor-tecadm"

---
+++ src/main/java/br/com/distrischool/professortecadm/DistrischoolProfessorTecadmServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.distrischool.professortecadm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrischoolProfessorTecadmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistrischoolProfessorTecadmServiceApplication.class, args);
+    }
+
+}
