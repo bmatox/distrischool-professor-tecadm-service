ffeca1c1443bfa1c903337a31f346a06015ad1ad
51644 <51644@tjce.jus.br>
2025-10-12 19:34:03 -0300

    feat: cria CreateProfessorRequest DTO e EmailAlreadyUsedException

---
+++ src/main/java/br/com/distrischool/professortecadm/dto/CreateProfessorRequest.java
@@ -0,0 +1,13 @@
+package br.com.distrischool.professortecadm.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+public record CreateProfessorRequest(
+        @NotBlank String nome,
+        @NotBlank @Email String email,
+        @NotBlank String especialidade,
+        @NotNull LocalDate dataContratacao
+) {}
---
+++ src/main/java/br/com/distrischool/professortecadm/exception/EmailAlreadyUsedException.java
@@ -0,0 +1,11 @@
+package br.com.distrischool.professortecadm.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EmailAlreadyUsedException extends RuntimeException {
+    public EmailAlreadyUsedException(String message) {
+        super(message);
+    }
+}
